9da275282d4333b123f3a111c83187e550305f88
Yahia-Derbala <yahderbala@gmail.com>
2023-12-19 15:40:38 +0200

    Assignment 8 Complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5d90a7e4d727c688479c6a223f525506266fb7db
+AESD_ASSIGNMENTS_VERSION = e42b53527c244ce48e748b97624acc4cca842aa8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,26 @@
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = d2cf881bb2a844e1389b2267b02b6f97da8eafa9
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Yahia-Derbala.git
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+AESDCHAR_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR) ARCH=$(KERNEL_ARCH)
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/modules-helpers/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/etc/modules-helpers/aesdchar_load
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/etc/modules-helpers/aesdchar_unload
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
